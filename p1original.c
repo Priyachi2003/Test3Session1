df2530fc032a9b382770901c9d8d9717a0e7a567
Priyachi2003 <96510349+Priyachi2003@users.noreply.github.com>
2022-03-02 15:49:01 +0000

    sum of two fractions

--- p1original.c
+++ p1original.c
@@ -12,20 +12,20 @@ void add(int num1,int den1, int num2, int den2, int *num3, int *den3)
   *den3=(den1*den2);
   for(i==1;i<=num3 && i<=den3;i++)
     {
-      if((num3)%i==0 && (den3)%i==0 )
+      if((*num3)%i==0 && (*den3)%i==0);
         int gcd_no=i;
     }
 }
 void output(int num1, int den1, int num2, int den2, int num3, int den3)
 {
-  printf("sum of fractions of %d,%d,%d,%d,%d,%d",num1,den1,num2,den2,num3,den3);
+  printf("sum of fractions of %d,%d,%d,%d is %d & %d",num1,den1,num2,den2,num3,den3);
 }
 int main()
 {
   int num1,den1,num2,den2,num3,den3;
   input(&num1,&den1,&num2,&den2);
   add(num1,den1,num2,den2,&num3,&den3);
-  output(int num1, int den1, int num2, int den2, int num3, int den3);
+  output(num1,  den1, num2, den2, num3, den3);
 return 0;
 }
 
